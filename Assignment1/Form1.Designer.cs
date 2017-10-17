7d2f599288f90e097162ce69f51458fffbc598c8
brian <briannippert@gmail.com>
2017-10-16 22:29:54 -0400

    Initial Commit

---
+++ Assignment1/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtStdDev = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMean = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPacketLoss = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Time";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 118);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Time";
+            series1.Name = "Ping";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(659, 457);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(93, 17);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(85, 13);
+            this.lblFile.TabIndex = 1;
+            this.lblFile.Text = "No File Selected";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minimuim:";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(69, 55);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.ReadOnly = true;
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 4;
+            // 
+            // txtStdDev
+            // 
+            this.txtStdDev.Location = new System.Drawing.Point(290, 80);
+            this.txtStdDev.Name = "txtStdDev";
+            this.txtStdDev.ReadOnly = true;
+            this.txtStdDev.Size = new System.Drawing.Size(100, 20);
+            this.txtStdDev.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Standard Deviation:";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(69, 80);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.ReadOnly = true;
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Maximum:";
+            // 
+            // txtMean
+            // 
+            this.txtMean.Location = new System.Drawing.Point(290, 51);
+            this.txtMean.Name = "txtMean";
+            this.txtMean.ReadOnly = true;
+            this.txtMean.Size = new System.Drawing.Size(100, 20);
+            this.txtMean.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(184, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Mean:";
+            // 
+            // txtPacketLoss
+            // 
+            this.txtPacketLoss.Location = new System.Drawing.Point(481, 80);
+            this.txtPacketLoss.Name = "txtPacketLoss";
+            this.txtPacketLoss.ReadOnly = true;
+            this.txtPacketLoss.Size = new System.Drawing.Size(100, 20);
+            this.txtPacketLoss.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(409, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Packet Loss:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 587);
+            this.Controls.Add(this.txtPacketLoss);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMean);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStdDev);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Ping Analyzer";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtStdDev;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMean;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPacketLoss;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignment1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        private string text;
+        private List<double> time = new List<double>();
+        private double count;
+        private int total = 1000;
+        private double min;
+        private double max;
+        private double average;
+        private double standardDev;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                count = 0;
+                min = 0;
+                max = 0;
+                average = 0;
+                standardDev = 0;
+                time.Clear();
+                text = "";
+                chart1.Series["Ping"].Points.Clear();
+            try
+            {
+                string path;
+                OpenFileDialog file = new OpenFileDialog();
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    lblFile.Text = file.FileName;
+                    path = file.FileName;
+                    text = File.ReadAllText(path);
+                    extractData(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR!");
+            }
+        }
+
+        private void extractData(string input)
+        {
+            Regex r1 = new Regex(@"time=(.*) ms");
+            r1.Matches(input);
+            foreach (Match m in r1.Matches(input))
+            {
+                time.Add(Convert.ToDouble(m.Groups[1].ToString()));
+            }
+            count = time.Count;
+            makeGraph();
+            generateStatistics();
+        }
+
+        private void makeGraph()
+        {
+            int count = 1;
+            double sum = 0;
+            foreach (double num in time)
+            {
+                sum += num;
+                chart1.Series["Ping"].Points.AddXY(count,num);
+                count++;
+            }
+            average = sum / count;
+            average = Math.Round(average,2);
+        }
+        private void generateStatistics()
+        {
+            time.Sort();
+            min = time[0];
+            time.Reverse();
+            max = time[0];
+            double sumOfSquaresOfDifferences = time.Select(val => (val - average) * (val - average)).Sum();
+            standardDev = Math.Round(Math.Sqrt(sumOfSquaresOfDifferences / time.Count),2);
+            txtMax.Text = max.ToString() + " ms";
+            txtMin.Text = min.ToString() + " ms";
+            txtMean.Text = average.ToString()+ " ms";
+            txtStdDev.Text = standardDev.ToString() + " ms";
+            txtPacketLoss.Text = ((1 - (count / 1000)) * 100).ToString() + "%";
+
+
+        }
+    }
+}
